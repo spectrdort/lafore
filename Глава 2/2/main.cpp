ea204d7bb2464e09784e20c7065d168180497a14
spectrdort <sms1995@rocketmail.com>
2021-07-27 19:37:20 +0300

    Напишите программу, выводящую следующую таблицу: 1990 135 1991 7290 1992 11300 1993 16200

---
+++ Глава 2/2/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	std::cout << std::setw(4) << 1990 << std::setw(10) << 135 << std::endl
+		<< std::setw(4) << 1991 << std::setw(10) << 7290 << std::endl
+		<< std::setw(4) << 1992 << std::setw(10) << 11300 << std::endl
+		<< std::setw(4) << 1993 << std::setw(10) << 16200 << std::endl;
+		return 0;
+}
