ef871be69fdeca781638b1d7d1745d53ad1fe26f
spectrdort <sms1995@rocketmail.com>
2021-07-27 20:00:23 +0300

    Операции ввода, такие, как cin, должны уметь преобразовывать последовательность символов в число. Напишите программу, которая позволяет пользователю ввести шесть цифр, а затем выводит результат типа long на экране. Каждая цифра должна считываться отдельно при помощи функции getche(). Вычисление значения переменной производится путем умножения текущего ее значения на 10 и сложения с последней введенной цифрой (для того, чтобы из кода символа получить цифру, вычтите из него 48 или '0'). Примером результата работы программы может служить следующий: Введите число: 123456 Вы ввели число 123456

---
+++ Глава 3/3/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <conio.h>
+
+int main()
+{
+	int x = 1, y = 0;
+	std::cout << "x = ";
+	for (int i = 100000; i >= 1;)
+	{
+		x = _getche() - 48;
+		y += x * i;
+		i /= 10;
+	}
+	std::cout << "\ny = " << y;
+}
