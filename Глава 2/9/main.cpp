7aa9f41b0837abf83b4b9693a59b0d963e0fbe42
spectrdort <sms1995@rocketmail.com>
2021-07-27 19:50:35 +0300

    Две дроби а/b и c/d можно сложить следующим образом: a/b + c/d = (a*d + b*c)/(b*d) Например, 1/4 + 2/3 = (1*3 + 4*2)/4*3 = 11/12 Напишите программу, запрашивающую у пользователя значения двух дробей, а затем выводящую результат, также записанный в форме дроби. Взаимодействие программы с пользователем может выглядеть, например, следующим образом: Введите первую дробь: 1/2 Введите вторую дробь: 2/5 Сумма равна 9/10 Вы можете использовать тот факт, что операция извлечения >> может считывать более одного значения

---
+++ Глава 2/9/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, c, d, chis, del;
+	char dummychar;
+	std::cout << "Enter first drob' ";
+	std::cin >> a >> dummychar >> b;
+	std::cout << "Enter second drob' ";
+	std::cin >> c >> dummychar >> d;
+	chis = (a * d) + (b * c);
+	del = b * d;
+	std::cout << "Sum is " << chis << "/" << del;
+}
