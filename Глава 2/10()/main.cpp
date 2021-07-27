32c08b5f4346995e935ef84b1d86ea119c4fae9b
spectrdort <sms1995@rocketmail.com>
2021-07-27 19:47:19 +0300

    Устаревшая денежная система Великобритании состояла из фунтов, шиллингов и пенсов. 1 фунт был равен 20 шиллингам, а 1 шиллинг — 12 пенсам. Для записи использовалась система, состоящая из знака £ и трех десятичных значений, разделенных точками. Например, запись £5.2.8 обозначала 5 фунтов, 2 шиллинга и 8 пенсов (пенс — множественное число от пенни). Современная денежная система, принятая в 50-е годы XX века, состоит только из фунтов и пенсов, причем один фунт равен 100 пенсам. Такой фунт называют десятичным. Таким образом, в новой денежной системе указанная сумма будет обозначаться как £5.13 (если быть точнее, £5.1333333). Напишите программу, которая будет преобразовывать сумму, записанную в старом формате (фунты, шиллинги, пенсы), в новый формат (фунты, пенсы). Форматом взаимодействия программы с пользователем может являться следующий: Введите количество фунтов: 7 Заедите количество шиллингов: 17 Введите количество пенсов: 9 Десятичных фунтов: £7.89 В большинстве компиляторов для представления знака £ используется десятичный код 156. Некоторые компиляторы позволяют скопировать знак фунта прямо из таблицы символов Windows.

---
+++ Глава 2/10()/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	float f, sh, p, new_pounds;
+	std::cout << "Pounds: ";
+	std::cin >> f;
+	std::cout << "Shilings: ";
+	std::cin >> sh;
+	std::cout << "Penni: ";
+	std::cin >> p;
+	new_pounds = f + (sh / 20) + (p / 20 / 12);
+	std::cout << "New pounds = J" << new_pounds;
+}
