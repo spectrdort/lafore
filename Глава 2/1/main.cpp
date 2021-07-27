a27d4d2771bfd0f198363ad2a886c7bafcc7ad2c
spectrdort <sms1995@rocketmail.com>
2021-07-27 19:35:57 +0300

    Считая, что кубический фут равен 7.481 галлона, написать программу, запрашивающую у пользователя число галлонов и выводящую на экран эквивалентный объем в кубических футах.

---
+++ Глава 2/1/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << setw(4) << 1990 << setw(6) << 135 << std::endl
+		<< setw(4) << 1991 << setw(6) << 7290 << std::endl
+		<< setw(4) << 1990 << setw(6) << 135 << std::endl
+		<< setw(4) << 1990 << setw(6) << 135 << std::endl
+	return 0;
+}
