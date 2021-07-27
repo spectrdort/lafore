81d8a7d82af620369e4ed27d15ee4f5ac3d7b92a
spectrdort <sms1995@rocketmail.com>
2021-07-27 19:49:42 +0300

    Напишите программу, выполняющую действия, обратные тем, которые описаны в упражнении 10, то есть запрашивающую у пользователя сумму, указанную в десятичных фунтах, и переводящую ее в старую систему фунтов, шиллингов и пенсов. Пример взаимодействия программы с пользователем может выглядеть так: Введите число десятичных фунтов: 3.51 Эквивалентная сумма в старой форме записи: £3.10.2 Обратите внимание на то, что если вам придется присваивать вещественное значение (например, 12.34) переменной целого типа, то его дробная часть (0.34) будет потеряна, а целая переменная получит значение 12. Чтобы избежать предупреждения со стороны компилятора, используйте явное преобразование типов. Можно использовать операторы, подобные приведенным ниже: float decpounds; // сумма в десятичных фунтах int pounds; // сумма в старых фунтах float decfrac; // десятичная дробная часть pounds = static_cast<int>(decpounds); // отбрасывание // дробной части decfrac = decpounds - pounds; // прибавление дробной части Чтобы получить число шиллингов, следует умножить на 20 значение переменной decfrac. Аналогичным образом можно получить число пенсов.

---
+++ Глава 2/12/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	int pounds, shillings, penny;
+	float new_pounds, new_shillings, new_penny;
+	std::cout << "New form = J";
+	std::cin >> new_pounds;
+	pounds = static_cast<int>(new_pounds);
+	new_shillings = (new_pounds - pounds) * 20;
+	shillings = static_cast<int>(new_shillings);
+	new_penny = (new_shillings - shillings) * 12;
+	penny = static_cast<int>(new_penny);
+	std::cout << "Old = J" << pounds << '.' << shillings << '.' << penny;
+}
