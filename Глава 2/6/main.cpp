3ddb899c97859e17c0567e798440d29ad496f47f
spectrdort <sms1995@rocketmail.com>
2021-07-27 19:42:57 +0300

    На биржевых торгах за 1 фунт стерлингов давали $1.487, за франк — $0.172, за немецкую марку — $0.584, а за японскую йену — $0.00955. Напишите программу, которая запрашивает денежную сумму в долларах, а затем выводит эквивалентные суммы в других валютах.

---
+++ Глава 2/6/main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	float funt = 1.487, frank = 0.172, marka = 0.584, yena = 0.00955, dollaar, funt_n, frank_n, marka_n, yena_n;
+	std::cin >> dollaar;
+	funt_n = dollaar * funt;
+	frank_n = dollaar * frank;
+	marka_n = dollaar * marka;
+	yena_n = dollaar * yena;
+	std::cout << funt_n << " " << frank_n << " " << marka_n << " " << yena_n;
+}
