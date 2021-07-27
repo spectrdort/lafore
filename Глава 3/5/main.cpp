db0bf4ccc7c623cf95e4c7bd74eccaa61ad36615
spectrdort <sms1995@rocketmail.com>
2021-07-27 20:04:24 +0300

    При помощи цикла for изобразите на экране пирамиду из символов 'X'. Верхняя часть пирамиды должна выглядеть следующим образом: х ххх ххххх ххххххх ххххххххх Вся пирамида должна быть высотой не 5 линий, как изображено здесь, а 20 линий. Одним из способов ее построения может служить использование двух вложенных циклов, из которых внутренний будет заниматься печатанием символов 'X' и пробелов, а другой осуществлять переход на одну строку вниз.

---
+++ Глава 3/5/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x;
+	std::cout << "Kolichestvvo strok: ";
+	std::cin >> x;
+	for (int j = 0; j < x; j++)
+	{
+		for (int i = 0; i < (x * 2); i++)
+		{
+			char ch = i >= x - j && i <= x + j ? 'X' : ' ';
+			std::cout << ch;
+		}
+		std::cout << std::endl;
+	}
+}
