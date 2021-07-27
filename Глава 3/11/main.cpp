786c1b7423c516caee4defe538dc1aa34201a3ee
spectrdort <sms1995@rocketmail.com>
2021-07-27 20:02:16 +0300

    Создайте калькулятор, выполняющий действия над денежными суммами, выраженными в фунтах, шиллингах и пенсах (см. упражнения 10 и 12 главы 2). Калькулятор должен складывать и вычитать вводимые значения, а также производить умножение денежной суммы на вещественное число (операция умножения двух денежных сумм не имеет смысла, поскольку квадратных денежных единиц не существует. Деление одной денежной суммы на другую мы тоже не будем рассматривать). Организация взаимодействия с калькулятором описана в упражнении 4 этой главы.

---
+++ Глава 3/11/main.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <conio.h>
+
+int main()
+{	
+	char ch, dch;
+	do
+	{
+		int p, p1 = 0, s, s1 = 0, f, f1 = 0, um;
+		std::cout << "First: "; std::cin >> p >> dch >> s >> dch >> f;
+		std::cout << "+\-\*: "; std::cin >> ch;
+		switch (ch)
+		{
+		case '+':
+			std::cout << "Second: "; std::cin >> p1 >> dch >> s1 >> dch >> f1;
+			f += f1;
+			if (f > 11)
+				s++;
+			s += s1;
+			if (s > 19)
+				p++;
+			p += p1;
+			break;
+		case '-':
+			std::cout << "Second: "; std::cin >> p1 >> dch >> s1 >> dch >> f1;
+			p -= p1;
+			if (s < s1)
+			{
+				p--;
+				s += 20 - s1;
+			}
+			else
+				s -= s1;
+			if (f < f1)
+			{
+				s--;
+				f += 12 - f1;
+			}
+			else
+				f -= f1;
+			break;
+		case '*':
+			std::cout << "Int num: ";  std::cin >> um;
+			f *= um;
+			while (f > 11)
+			{
+				f -= 12;
+				s1++;
+			}
+			s *= um;
+			while (s > 19)
+			{
+				s -= 20;
+				p1++;
+			}
+			s += s1;
+			if (s > 19)
+			{
+				s -= 20;
+				p1++;
+			}
+			p *= um;
+			p += p1;
+		}
+		std::cout << "Result: " << p << dch << s << dch << f << "\nAnother one (y/n) ";
+		dch = _getche();
+		std::cout << std::endl;
+	} while (dch == 'y');
+}
