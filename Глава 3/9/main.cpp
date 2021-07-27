93289e3a51f2b6a2ba90cc5714160118f1d818e6
spectrdort <sms1995@rocketmail.com>
2021-07-27 20:07:31 +0300

    Представьте, что вы собираетесь пригласить к себе шестерых гостей, но за вашим столом могут разместиться всего лишь 4 человека Сколькими способами можно разместить четырех из шести гостей за обеденным столом? Каждый из шести гостей может разместиться на первом стуле. Каждый из оставшихся пяти гостей может занять второй стул. На третьем стуле может разместиться один их четырех гостей, и на четвертом — один из трех оставшихся гостей. Двоим из гостей не достанется ни одного места. Таким образом, число возможных рассадок гостей за столом равно 6*5*4*3 = 360. Напишите программу, которая будет производить аналогичные вычисления для любого числа гостей и любого числа мест за столом (при этом предполагается, что число гостей не меньше числа мест). Программа не должна быть сложной, и вычисление можно организовать с помощью простого цикла for.

---
+++ Глава 3/9/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main()
+{
+	int chairs, guests, res = 1;
+	std::cout << "Num of guests: "; std::cin >> guests;
+	std::cout << "Num of chairs: "; std::cin >> chairs;
+	for (int i = guests; i >= (chairs - 1); --i)
+	{
+		res *= i;
+	}
+	std::cout << "Result: " << res << std::endl;
+}
