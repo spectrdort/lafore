ef132169eb02155c0eb9d3088197817c933876ff
spectrdort <sms1995@rocketmail.com>
2021-07-27 20:05:05 +0300

    Измените программу factor, приведенную в этой главе, таким образом, чтобы она циклически запрашивала ввод пользователем числа и вычисляла его факториал, пока пользователь не введет 0. В этом случае программа должна завершиться. При необходимости вы можете использовать соответствующие операторы программы factor в цикле do или while.

---
+++ Глава 3/6/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+
+int main()
+{
+	unsigned int numb, ch;
+	unsigned long fact = 1;
+	do
+	{
+		std::cout << "Ent numb ";
+		std::cin >> numb;
+		for (int i = numb; i > 0; i--)
+		{
+			fact *= i;
+		}
+		std::cout << "Fact = " << fact << std::endl;
+		fact = 1;
+		std::cout << "Another one (1/0) ";
+		ch = _getche() - 48;
+		std::cout << std::endl;
+	} while (ch != 0);
+	return 0;
+}
