b5acbabe5c8f9afdbb3e6651fc0704eb2bf50170
spectrdort <sms1995@rocketmail.com>
2021-07-29 22:49:26 +0300

    Расположение точки на плоскости можно задать с помощью двух координат: x и y. Например, если точка имеет координаты (4, 5), то это значит, что она отстоит на 4 единицы справа от вертикальной оси и на 5 единиц вверх от горизонтальной оси. Сумма двух точек определяется как точка, имеющая координаты, равные сумме соответствующих координат слагаемых. Напишите программу, использующую для интерпретации точки на плоскости структуру с названием point. Определите три переменные типа point, и две из них инициализируйте с помощью значений, вводимых с клавиатуры. Затем присвойте третьей переменной значение суммы первых двух переменных и выведите результат на экран. Результат работы программы может выглядеть следующим образом: Введите координаты точки p1: 3 4 Введите координаты точки p2: 5 7 Координаты точки p1 + p2 равны 8.11

---
+++ Глава 4/12/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+struct fraction
+{
+	int c;
+	int z;
+};
+
+int main()
+{
+	fraction f1, f2, f3;
+	char ch, dch;
+	do
+	{
+		std::cout << "Enter first fraction:"; std::cin >> f1.c >> dch >> f1.z;
+		std::cout << "Enter znak: (+,-,*,/)"; std::cin >> ch;
+		std::cout << "Enter second fraction:"; std::cin >> f2.c >> dch >> f2.z;
+		switch (ch)
+		{
+		case '+':
+			f3.c = (f1.c * f2.z) + (f1.z * f2.c);
+			f3.z = f1.z * f2.z;
+			break;
+		case '-':
+			f3.c = (f1.c * f2.z) - (f1.z * f2.c);
+			f3.z = f1.z * f2.z;
+			break;
+		case '*':
+			f3.c = f1.c * f2.c;
+			f3.z = f1.z * f2.z;
+			break;
+		case '/':
+			f3.c = f1.c * f2.z;
+			f3.z = f1.z * f2.c;
+			break;
+		default:
+			break;
+		}
+		std::cout << "Result is: " << f3.c << dch << f3.z << std::endl;
+		std::cout << "Another one? (y/n) "; std::cin >> ch;
+		std::cout << std::endl;
+	} while (ch == 'y');
+}
