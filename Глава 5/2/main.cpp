4896b3b4f023b5cbaf2827e83b0d8a192b26d0c8
spectrdort <sms1995@rocketmail.com>
2021-07-29 22:45:58 +0300

    Возведение числа n в степень р — это умножение числа n на себя р раз. Напишите функцию с именем power(), которая в качестве аргументов принимает значение типа double для n и значение типа int для р и возвращает значение типа double. Для аргумента, соответствующего степени числа, задайте значение по умолчанию, равное 2, чтобы при отсутствии показателя степени при вызове функции число n возводилось в квадрат. Напишите функцию main(), которая запрашивает у пользователя ввод аргументов для функции power(), и отобразите на экране результаты ее работы.

---
+++ Глава 5/2/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+double power(double& n, int p = 2);
+
+int main()
+{	
+	double n;
+	int p;
+	std::cin >> n >> p;
+	std::cout << power(n, p);
+}
+
+double power(double& n, int p)
+{
+	double s = n;
+	for (int i = 0; i < (p - 1); i++)
+	{
+		n *= s;
+	}
+	return n;
+}
