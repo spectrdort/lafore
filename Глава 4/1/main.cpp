0808e6e2b5af8d22c122f623ec3ccb34b7ecab61
spectrdort <sms1995@rocketmail.com>
2021-07-29 22:47:12 +0300

    Номер телефона, например (212) 767—8900, можно условно разделить на  три части: код города (212), номер телефонной станции (767) и номер  абонента (8900). Напишите программу с использованием структуры, поз-  воляющую раздельно хранить эти три части телефонного номера. Назо-  вите структуру phone. Создайте две структурные переменные типа phone.  Инициализацию одной из них произведите сами, а значения для другой  запросите с клавиатуры. Затем выведите содержимое обеих переменных  на экран. Результат работы программы должен выглядеть приблизитель-  но так: Введите код города, номер станции и номер абонента: 415 555 1212 Мой номер (212) 767-8900 Ваш номер (415) 555-1212

---
+++ Глава 4/1/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+struct phone
+{
+	int first;
+	int second;
+	int third;
+};
+
+int main()
+{
+	phone mine{ 212, 767, 8900 };
+	phone yours;
+	std::cout << "Int ur num: "; std::cin >> yours.first >> yours.second >> yours.third;
+	std::cout << "My num: (" << mine.first << ") " << mine.second << "-" << mine.third << std::endl;
+	std::cout << "Ur num: (" << yours.first << ") " << yours.second << "-" << yours.third << std::endl;
+}
