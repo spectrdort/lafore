d4ae724d9fe0415f199b4add1d956a654842eaeb
spectrdort <sms1995@rocketmail.com>
2021-07-29 22:44:58 +0300

    Вернитесь к рассмотрению примера CIRCAREA главы 2 «Основы программирования на C++». Напишите функцию с именем circarea(), которая аналогичным образом вычисляет площадь круга. Функция должна принимать один аргумент типа float и возвращать значение типа float. Напишите функцию main(), которая просит пользователя ввести значение радиуса, вызывает функцию circarea(), а затем отображает результат вычисления на экране.

---
+++ Глава 5/1/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <conio.h>
+
+int main()
+{
+	float circarea(float& rad);
+	float rad;
+	std::cin >> rad;
+	std::cout << circarea(rad);
+}
+
+float circarea(float& rad)
+{
+	return 3.14159 * rad * rad;
+}
