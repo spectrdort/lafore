52dfc1e14f3786a4a55985099b1de2eee0970d84
spectrdort <sms1995@rocketmail.com>
2021-07-30 18:08:24 +0300

    Создайте класс employee, используя упражнение 4 главы 4. Класс должен включать поле типа int для хранения номера сотрудника и поле типа float для хранения величины его оклада. Методы класса должны позволять пользователю вводить и отображать данные класса. Напишите функцию main(), которая запросит пользователя ввести данные для трех сотрудников и выведет полученную информацию на экран.

---
+++ Глава 6/04/main.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+
+class employee
+{
+private:
+	int num;
+	float cash;
+public:
+	employee(): num(0), cash(0.0)
+	{
+
+	}
+	void add(int& n, float& c)
+	{
+		num = n;
+		cash = c;
+	}
+	void print()
+	{
+		std::cout << "Num of employee: " << num << " Cash: " << cash << std::endl;
+	}
+};
+
+void add_n(employee&, employee&, employee&);
+void print_n(employee&, employee&, employee&);
+
+
+int main()
+{
+	employee e1, e2, e3;
+	add_n(e1, e2, e3);
+	print_n(e1, e2, e3);
+	return 0;
+}
+
+void add_n(employee& e1, employee& e2, employee& e3)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		int n;
+		float c;
+		std::cout << "Enter num of emp: ";  std::cin >> n;
+		std::cout << "Enter cash of emp: ";  std::cin >> c;
+		switch (i)
+		{
+		case 0:
+			e1.add(n, c);
+			break;
+		case 1:
+			e2.add(n, c);
+			break;
+		case 2:
+			e3.add(n, c);
+			break;
+		default:
+			break;
+		}
+	}
+}
+void print_n(employee& e1, employee& e2, employee& e3)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		switch (i)
+		{
+		case 0:
+			e1.print();
+			break;
+		case 1:
+			e2.print();
+			break;
+		case 2:
+			e3.print();
+			break;
+		default:
+			break;
+		}
+	}
+}
